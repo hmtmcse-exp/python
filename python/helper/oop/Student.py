d5c3c0f3d2479dcf17e4e8a9854ce4ffbb30a46f
Touhid Mia <touhid@bitmascot.com>
2017-02-19 16:24:57 +0600

    ignore

---
+++ python/helper/oop/Student.py
@@ -0,0 +1,14 @@
+class Student:
+    count = 0
+
+    def __init__(self,count):
+        self.count = count
+
+
+    def displayCount(self):
+        print("Total Student %d" % self.count)
+
+
+
+student = Student(10)
+student.displayCount()
