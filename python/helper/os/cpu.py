dd968a1efefad024d9bb90299ded2165d66f9758
Touhid Mia <touhid@bitmascot.com>
2017-02-20 08:19:50 +0600

    ignore

---
+++ python/helper/os/cpu.py
@@ -0,0 +1,13 @@
+import os
+
+def get_cpu_load():
+    """ Returns a list CPU Loads"""
+    result = []
+    cmd = "WMIC CPU GET LoadPercentage "
+    response = os.popen(cmd + ' 2>&1','r').read().strip().split("\r\n")
+    for load in response[1:]:
+       result.append(int(load))
+    return result
+
+if __name__ == '__main__':
+    print(get_cpu_load())
