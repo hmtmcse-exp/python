3be4640b91f00bff2d04532468cc93061bcff010
Touhid Mia <hmtm.cse@gmail.com>
2017-03-22 22:06:00 +0600

    Custom UI

--- django/custom_ui/servers/urls.py
+++ django/custom_ui/servers/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from . import views
 
-from servers import views
-
-urlpatterns = patterns('',
-  url(r'^$', views.ServerList.as_view(), name='server_list'),
-  url(r'^new$', views.ServerCreate.as_view(), name='server_new'),
-  url(r'^edit/(?P<pk>\d+)$', views.ServerUpdate.as_view(), name='server_edit'),
-  url(r'^delete/(?P<pk>\d+)$', views.ServerDelete.as_view(), name='server_delete'),
-)
+urlpatterns = [
+    url(r'^$', views.server_list, name='server_list'),
+    url(r'^new$', views.server_create, name='server_new'),
+    url(r'^edit/(?P<pk>\d+)$', views.server_update, name='server_edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.server_delete, name='server_delete'),
+]
