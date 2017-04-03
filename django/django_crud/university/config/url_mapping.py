f9fb300a3501fd9661edda88f4b39b353331685c
Touhid Mia <touhid@bitmascot.com>
2017-04-03 17:44:24 +0600

    init

--- django/django_crud/university/config/url_mapping.py
+++ django/django_crud/university/config/url_mapping.py
@@ -5,7 +5,7 @@ from university.controllers import home
 from university.services import URL_CONSTANT
 
 urlpatterns = [
-    url(r'^', home.home, name='home'),
+    url(r'^' + URL_CONSTANT.HOME_INDEX, home.home, name='home'),
     url(r'^' + URL_CONSTANT.COURSE_INDEX, course.index, name='index'),
     url(r'^' + URL_CONSTANT.COURSE_CREATE, course.create, name='create'),
     url(r'^' + URL_CONSTANT.COURSE_UPDATE, course.update, name='update'),
--- django/django_crud/university/services/URL_CONSTANT.py
+++ django/django_crud/university/services/URL_CONSTANT.py
@@ -3,3 +3,4 @@ COURSE_CREATE = "course/create/"
 COURSE_UPDATE = "course/update/"
 COURSE_DELETE = "course/delete/"
 
+HOME_INDEX = ""
