fdf397a7aa1ffcabfff253aef0dad44a34180cc5
Touhid Mia <touhid@bitmascot.com>
2017-04-03 16:15:20 +0600

    init

--- django/django_crud/university/models.py
+++ django/django_crud/university/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    app_uuid = models.UUIDField(default=uuid.uuid4)
+    created = models.DateTimeField(auto_now=True)
+    enable = models.BooleanField(default=True)
