b8b257d66295b2caded977fb688e208d49396d07
Touhid Mia <hmtm.cse@gmail.com>
2017-04-03 20:26:37 +0600

    Template

--- django/django_crud/django_crud/settings.py
+++ django/django_crud/django_crud/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'university'
+    'django_crud',
+    'university',
 ]
 
 MIDDLEWARE = [
---
+++ django/django_crud/django_crud/templatetags/ui_helper_extras.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.html import format_html
+
+register = template.Library()
+
+
+def concat_static_url(url):
+    return "/static/" + url
+
+
+@register.simple_tag
+def load_css(file_name):
+    url = concat_static_url("css/" + file_name)
+    return format_html('<link rel="stylesheet" href="' + url + '">')
+
+
+@register.simple_tag
+def load_js(file_name):
+    url = concat_static_url("js/" + file_name)
+    return format_html('<script src="' + url + '"></script>')
+
+
+@register.simple_tag
+def image_url(file_name):
+    url = concat_static_url("images/" + file_name)
+    return url
+
