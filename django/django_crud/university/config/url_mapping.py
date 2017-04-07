08ac2eac9d077f882617002f659fa628936296ab
Touhid Mia <hmtm.cse@gmail.com>
2017-04-07 16:29:47 +0600

    ignore

--- django/django_crud/university/config/url_mapping.py
+++ django/django_crud/university/config/url_mapping.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from university.controllers import course, department
+from university.controllers import course, department, student
 from university.controllers import home
 from university.services import URL_CONSTANT
 
@@ -17,6 +17,15 @@ urlpatterns = [
     url(r'^' + URL_CONSTANT.DEPARTMENT_DELETE + "(?P<pk>\d+)/?$", department.delete, name='department_delete'),
     url(r'^' + URL_CONSTANT.DEPARTMENT_DETAILS + "(?P<pk>\d+)/?$", department.details, name='department_details'),
     url(r'^' + URL_CONSTANT.DEPARTMENT_INDEX + "?$", department.index, name='department_index'),
+
+    url(r'^' + URL_CONSTANT.STUDENT_EDIT + "(?P<pk>\d+)/?$", student.edit, name='department_edit'),
+    url(r'^' + URL_CONSTANT.STUDENT_CREATE + "?$", student.create, name='department_create'),
+    url(r'^' + URL_CONSTANT.STUDENT_SAVE + "?$", student.save, name='department_save'),
+    url(r'^' + URL_CONSTANT.STUDENT_UPDATE + "?$", student.update, name='department_update'),
+    url(r'^' + URL_CONSTANT.STUDENT_DELETE + "(?P<pk>\d+)/?$", student.delete, name='department_delete'),
+    url(r'^' + URL_CONSTANT.STUDENT_DETAILS + "(?P<pk>\d+)/?$", student.details, name='department_details'),
+    url(r'^' + URL_CONSTANT.STUDENT_INDEX + "?$", student.index, name='department_index'),
+
 
     url(r'^' + URL_CONSTANT.HOME_INDEX, home.home, name='home'),
 ]
--- django/django_crud/university/models.py
+++ django/django_crud/university/models.py
@@ -29,6 +29,17 @@ class Course(models.Model):
     created = models.DateTimeField(auto_now=True)
     enable = models.BooleanField(default=True)
 
+    @staticmethod
+    def get_all_fields():
+        return {
+            'name': {
+                'required': True,
+                'message': 'Please Enter Name.'
+            },
+            'description': {},
+            'enable': {},
+        }
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=200)
@@ -38,5 +49,16 @@ class Student(models.Model):
     app_uuid = models.UUIDField(default=uuid.uuid4)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course)
+
+    @staticmethod
+    def get_all_fields():
+        return {
+            'first_name': {
+                'required': True,
+                'message': 'Please Enter First Name.'
+            },
+            'last_name': {},
+            'enable': {},
+        }
 
 
